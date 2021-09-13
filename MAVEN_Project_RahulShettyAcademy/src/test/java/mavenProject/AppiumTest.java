73e3995dd9f079557e8450582c64bd5122822ae2
santoshparida1985 <santosh.parida13@gmail.com>
2021-09-13 21:13:10 +0530

    Demo2 workspace commit

---
+++ MAVEN_Project_RahulShettyAcademy/src/test/java/mavenProject/AppiumTest.java
@@ -0,0 +1,22 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	@Test
+	public void APPIUMBrowserAutomation()
+	{
+		
+		System.out.println("APPIUM Browser Automation");
+	}
+	
+	@Test
+	public void APPIUMUIAutomation()
+	{
+		
+		System.out.println("APPIUM UI Automation ");
+	}
+
+}
