22e5e6fef9a8f7b755f3f38f9dd2e59ece8ca26a
santoshparida1985 <santosh.parida13@gmail.com>
2021-09-13 22:06:24 +0530

    Second Commit

---
+++ MAVEN_Project_RahulShettyAcademy/src/test/java/mavenProject/RestAssuredTest.java
@@ -0,0 +1,22 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+	
+	
+	@Test
+	public void APIBrowserAutomation()
+	{
+		
+		System.out.println("API Browser automation");
+	}
+	
+	@Test
+	public void APIUIAutomation()
+	{
+		
+		System.out.println("API UI automation");
+	}
+
+}
---
+++ MAVEN_Project_RahulShettyAcademy/src/test/java/mavenProject/SeleniumTest.java
@@ -0,0 +1,24 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	
+	
+	@Test
+	public void SeleniumBrowserAutomation()
+	{
+		
+		System.out.println("Selenium Browser automation");
+	}
+
+	
+	@Test
+	public void SeleniumUIAutomation()
+	{
+		
+		System.out.println("Selenium UI automation");
+	}
+}
